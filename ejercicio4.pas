14844628d727d4ee857ff26247083d7d0d7aeb96
Sfolgueras <sfolgueras@nigul.cide.es>
2023-05-04 15:51:59 +0200

    Mi cuarto ejercicio

---
+++ ejercicio4.pas
@@ -0,0 +1,12 @@
+program ejercicio4;
+var
+    n1 : integer;
+    n2 : integer;
+begin
+  writeln('¿Cual es el primer numero?');
+  readln(n1);
+  writeln('¿Y el segundo?');
+  readln(n2);
+  writeln('La suma es:');
+  write(n1+n2);
+end.
