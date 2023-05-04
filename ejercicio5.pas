450c3c779e3daf4dbacad4c9c54fc4e9c2a9c627
Sfolgueras <sfolgueras@nigul.cide.es>
2023-05-04 16:00:49 +0200

    Mi cuarto ejercicio

---
+++ ejercicio5.pas
@@ -0,0 +1,9 @@
+program ejercicio5;
+var
+    n : integer;
+begin
+  writeln('Dame un numero y te diré si es positivo o no');
+  readln(n);
+  if n > 0 then writeln('Es positivo')
+  else writeln('Es negativo o 0');
+end.
